f7c905c9471e93e77418af96c8e26d7b761f9243
Syed Sikandar shah <s.sikandar.sa@gmail.com>
2025-08-21 07:58:57 +0000

    i could not find .env file in the code

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  // IMPORTANT: Replace with your Firebase project's configuration
-  apiKey: "AIzaSyDz8SwicxbG06oAivJkdipAbmnC7bREChA",
-  authDomain: "hosteldesk-93tgb.firebaseapp.com",
-  projectId: "hosteldesk-93tgb",
-  storageBucket: "hosteldesk-93tgb.firebasestorage.app",
-  messagingSenderId: "21622555201",
-  appId: "1:21622555201:web:db6116096ddfa914b1fcc0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
